9a3d46292b88596ce19846c05cb9c45bca036424
moizgittpro <Abdulmoiz27j@gmail.com>
2025-05-14 07:49:33 +0500

    added frontend origin

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ app = FastAPI(lifespan=lifespan)
 # Middleware for CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000, https://tourism-omega-flame.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
