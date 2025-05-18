a951076839c8a43857d6d277ba20329201461af2
Khubaib Ahmed <kahmed.bscs23seecs@seecs.edu.pk>
2025-05-18 18:40:53 +0500

    fixed step

--- backend/chatbot/chatbot_chaining.py
+++ backend/chatbot/chatbot_chaining.py
@@ -482,7 +482,7 @@ async def chat(request : Request):
             if response["status"] == "success":
                 user_state["current_step"] = "mood"
                 states["days"] = user_input
-                session["states"]["origin"] = user_input
+                session["states"]["days"] = user_input
                 template = response["template"]
                 return_json = model.invoke(template).content
             else:
@@ -493,7 +493,7 @@ async def chat(request : Request):
             if response["status"] == "success":
                 user_state["current_step"] = "route"
                 states["mood"] = user_input
-                session["states"]["origin"] = user_input
+                session["states"]["mood"] = user_input
                 template = response["template"]
                 return_json = model.invoke(template).content
             else:
@@ -503,7 +503,7 @@ async def chat(request : Request):
             response = get_route(user_input)
             if response["status"] == "success":
                 states["route"] = user_input
-                session["states"]["origin"] = user_input
+                session["states"]["route"] = user_input
                 summary_response = generate_summary()
                 if summary_response["status"] == "success":
                     print("AFTER SUMMARY")
@@ -525,7 +525,7 @@ async def chat(request : Request):
         elif step == "chat":
             response = get_chat_response(user_input)
             user_state["current_step"] = "chat" ## REMAIN IN CHAT 
-            session["states"]["origin"] = user_input
+            session["states"]["chat"] = user_input
 
             if response["status"] == "success":
                 return_json = response.get("output", "No response generated.")
